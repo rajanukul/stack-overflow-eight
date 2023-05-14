8db9ac29b56fa80d14744dd0fa82014011e946d8
Anukul Raj <85643622+rajanukul@users.noreply.github.com>
2023-05-15 03:40:00 +0530

    Update AboutAuth.jsx

--- src/Pages/Auth/AboutAuth.jsx
+++ src/Pages/Auth/AboutAuth.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutAuth = () => {
   return (
     <div className="auth-container-1">
-      <h1>Join the Stack Overflow community</h1>
+      <h1>Join the ChitaChat community</h1>
       <p>Get unstuck — ask a question</p>
       <p>Unlock new privileges like voting and commenting</p>
       <p>Save your favorite tags, filters, and jobs</p>
